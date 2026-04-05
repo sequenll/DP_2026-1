7dc36ea6bdb5da2c7989ba369e90c45604666f3b
전민서 <waterleaf.duksung.ac.kr>
2026-04-05 17:11:37 +0900

    4월2일 수업종료

---
+++ src/practice/ch05/Singleton.java
@@ -0,0 +1,17 @@
+package practice.ch05;
+
+// 싱글턴 패턴 적용하기
+public class Singleton {
+    //(3) SIngleton 객체를 미리 만들어서 static 변수에 저장해 놓는다.
+    private static Singleton singleton = new Singleton(); // 클래스 로드 시 실행됨
+
+    // (1) 생성자를 private 으로 한다.
+    private Singleton() {
+        System.out.println("Singleton 객체가 생성됩니다.");
+    }
+
+    // (2) Singleton 객체를 얻어가는  static 메소드를 정의한다.
+    public static Singleton getInstance() {
+        return singleton;
+    }
+}
---
+++ src/practice/ch06/Main.java
@@ -0,0 +1,9 @@
+package practice.ch06;
+
+public class Main {
+    public static void main(String[] args) {
+        // 메인 메서드 내용
+        MessageBox mbox1 = new MessageBox('*');
+        mbox1.use("Hello, world.");
+    }
+}
