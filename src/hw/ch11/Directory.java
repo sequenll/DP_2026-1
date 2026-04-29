f307cbc1da98447ed862a8faf5891acee0b146c0
전민서 <waterleaf.duksung.ac.kr>
2026-04-29 17:22:00 +0900

    11장 과제

---
+++ src/hw/ch11/Directory.java
@@ -0,0 +1,53 @@
+package hw.ch11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Directory extends Entry {
+    private String name;
+    private List<Entry> directory = new ArrayList<>();
+
+    public Directory(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getSize() {
+        int size = 0;
+        for (Entry entry: directory) {
+            size += entry.getSize();
+        }
+        return size;
+    }
+
+    @Override
+    protected void printList(String prefix) {
+        System.out.println(prefix + "/" + this);
+        for (Entry entry: directory) {
+            entry.printList(prefix + "/" + name);
+        }
+    }
+
+    public Entry add(Entry entry) {
+        directory.add(entry); // 인자로 들어온 자식을 추가
+        entry.setParent(this); // 현재 디렉토리를 인자로 들어온 자식의 부모로 설정
+        return this;
+    }
+
+    @Override
+    public List<Entry> search(String keyword) {
+        List<Entry> result = new ArrayList<>();
+        if(name.contains(keyword)) {
+            result.add(this);
+        }
+        for (Entry entry: directory) {
+            result.addAll(entry.search(keyword));
+        }
+        return result;
+    }
+}
