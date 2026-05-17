0765cbe63dce1e14fb89015a37e9bae1e6615f3c
전민서 <waterleaf.duksung.ac.kr>
2026-05-17 14:14:29 +0900

    16장 과제

---
+++ src/hw/ch16/Main.java
@@ -0,0 +1,9 @@
+package hw.ch16;
+
+public class Main {
+    static public void main(String[] args) {
+        System.out.println("학번: 20240770, 이름: 전민서");
+
+        new LoginFrame("Mediator Sample");
+    }
+}
